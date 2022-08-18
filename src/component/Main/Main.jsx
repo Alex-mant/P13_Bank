9a47daff43747f1d9b824884fc05f5a7e97c9df3
Alex Mant <al.mantione@gmail.com>
2022-08-18 12:32:08 +0200

    feat(Application/src/component/Main):
    set up the Main jsx component and its style

---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Features from '../Features/Features';
+import Hero from '../Hero/Hero';
+import './styles.scss'
+
+const Main = () => {
+  return (
+    <main>
+      <Hero/>
+      <Features/>
+    </main>
+  );
+}
+
+export default Main;
