7ed92fc57499aea310fa370245fcaf22a87b642f
Alex Mant <al.mantione@gmail.com>
2022-08-18 12:31:01 +0200

    feat(Application/src/component/FeatureItem):
    set up the FeatureItem jsx component and its style

---
+++ src/component/FeatureItem/FeatureItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.scss';
+
+const FeatureItem = ({imageURL, title, text}) => {
+  return (
+    <div className="feature-item">
+      <img src={imageURL} alt="Chat Icon" className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default FeatureItem;
