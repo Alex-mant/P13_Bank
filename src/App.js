e69d93585460c469ae25da23506cb921c8aedcd7
Alex Mant <al.mantione@gmail.com>
2022-08-18 12:22:31 +0200

    feat(App.js):
    Router and Routes has been added,

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Footer from "./component/Footer/Footer";
+import Nav from "./component/Nav/Nav";
+import Index from "./pages/index";
+import SignIn from "./pages/sign-in";
+import User from "./pages/user";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route exact path='/home' element={<Index/>}/>
+          <Route path='/signIn' element={<SignIn/>}/>
+          <Route path='/user' element={<User/>}/>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
