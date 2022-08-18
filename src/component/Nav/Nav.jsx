c9cfa8e9d7ba99add1cf42d3202b0309cde69aa7
Alex Mant <al.mantione@gmail.com>
2022-08-18 12:45:23 +0200

    refactor(application/src/component/Nav):
    Modify links redirected by href

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import headerLogo from "../../assets/img/argentBankLogo.png";
+import ArgentBankLogo from "../../assets/img/argentBankLogo.png";
 import "./styles.scss";
 
 const Nav = () => {
   return (   
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
-        <img className="main-nav-logo-image" src={headerLogo} alt="Argent Bank Logo" />
+      <a className="main-nav-logo" href="/home">
+        <img className="main-nav-logo-image" src={ArgentBankLogo} alt="Argent Bank Logo" />
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <a className="main-nav-item" href="/signIn">
           <i className="fa fa-user-circle"></i>
           Sign In
         </a>
