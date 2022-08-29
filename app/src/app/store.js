b15feec858d7c818a1d5451351c6c834d070082a
Alex Mant <al.mantione@gmail.com>
2022-08-30 00:29:42 +0200

    mise en place du store redux

---
+++ app/src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { loginSlice } from "../feature/loginSlice";
+
+/* Creating a store with the reducer. */
+export  const store = configureStore({
+  reducer: {
+    login: loginSlice.reducer
+  },
+})
