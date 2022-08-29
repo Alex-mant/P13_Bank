7e99b7c3fc9f4cec50cee2da850a7a0015278a22
Alex Mant <al.mantione@gmail.com>
2022-08-29 10:20:12 +0200

    refactor(SignInForm):
    Replace onChange to useRef method

--- app/src/components/MainSignInForm/MainSignInForm.jsx
+++ app/src/components/MainSignInForm/MainSignInForm.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import {  useNavigate  } from "react-router-dom";
 import { login } from "../../services/auth.service";
 
 import "./styles.scss";
 
 const MainSignInForm = ({setUserDataStorage}) => {
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
+  const emailInput = useRef();
+  const passwordInput = useRef();
   // eslint-disable-next-line no-unused-vars
   const [token, setToken] = useState(null)
   const navigate = useNavigate();
@@ -14,26 +15,25 @@ const MainSignInForm = ({setUserDataStorage}) => {
   
   const handleLogin = (e) => {
     e.preventDefault();
-    login(username, password, setToken, navigate, setUserDataStorage);
+    login(emailInput, passwordInput, setToken, navigate, setUserDataStorage);
   }
 
   return (
     <form onSubmit={handleLogin}>
       <div className="input-wrapper">
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" onChange={(e) => {setUsername(e.target.value)}}/>
+        <input type="text" id="username" ref={emailInput}/>
         <div className="email-error" style={{color: 'red'}}></div>
       </div>
       <div className="input-wrapper">
         <label htmlFor="password">Password</label>
-        <input type="current-password" id="password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <input type="current-password" id="password" ref={passwordInput}/>
         <div className="password-error" style={{color: 'red'}}></div>
       </div>
       <div className="input-remember">
         <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-
       <button className="sign-in-button">Sign In</button>
     </form>
   );
--- app/src/services/auth.service.js
+++ app/src/services/auth.service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const myElement = (element) => document.querySelector(element);
-const API_BASE_URL = 'http://localhost:3001/api/v1/user/'
+const API_BASE_URL = "http://localhost:3001/api/v1/user/";
 
 export const register = (firstName, lastName, email, password) => {
-  return axios.post(API_BASE_URL + 'signup', {
+  return axios.post(API_BASE_URL + "signup", {
     firstName,
     lastName,
     email,
@@ -12,45 +12,49 @@ export const register = (firstName, lastName, email, password) => {
   });
 };
 
-export const login = (username, password, setToken, navigate, storage) => {
-
-  axios.post(API_BASE_URL + 'login',{ email: username, password})
-  .then((res) => {
-    setToken(res.data.body.token);
-    if(res.data.body.token){
-      localStorage.setItem("user", JSON.stringify(res.data))
-      getUserData(res, navigate, storage)
-    }
-  })
-  .catch((err) => {
-    const error = err.response.data.message;
-    localStorage.removeItem("user");
-    if(error === 'Error: User not found!'){
-      myElement(".email-error").innerHTML = error
-      myElement(".password-error").innerHTML = ''
-    }else{
-      myElement(".email-error").innerHTML = ''
-      myElement(".password-error").innerHTML = error
-    }
-    if(error === "Error: data and hash arguments required"){
-      myElement(".password-error").innerHTML = "Error: Password is required!"
-    }
-  })
-  return true
-}
+export const login = (email, password, setToken, navigate, storage) => {
+  axios
+    .post(API_BASE_URL + "login", {
+      email: email.current.value,
+      password: password.current.value,
+    })
+    .then((res) => {
+      setToken(res.data.body.token);
+      if (res.data.body.token) {
+        localStorage.setItem("user", JSON.stringify(res.data));
+        getUserData(res, navigate, storage);
+      }
+    })
+    .catch((err) => {
+      const error = err.response.data.message;
+      localStorage.removeItem("user");
+      if (error === "Error: User not found!") {
+        myElement(".email-error").innerHTML = error;
+        myElement(".password-error").innerHTML = "";
+      } else {
+        myElement(".email-error").innerHTML = "";
+        myElement(".password-error").innerHTML = error;
+      }
+      if (error === "Error: data and hash arguments required") {
+        myElement(".password-error").innerHTML = "Error: Password is required!";
+      }
+    });
+  return true;
+};
 
 const getUserData = (res, navigate, storage) => {
-  axios.defaults.headers.common['Authorization'] = 'Bearer' + res.data.body.token
-  axios.post(API_BASE_URL + 'profile')
-  .then((resProfile) => {
-    storage(resProfile.data);
-    navigate("/user/profile")
-  })
-  .catch((errProfile) => {
-    console.log(errProfile)
-  })
-}
+  axios.defaults.headers.common["Authorization"] = "Bearer" + res.data.body.token;
+  axios
+    .post(API_BASE_URL + "profile")
+    .then((resProfile) => {
+      storage(resProfile.data);
+      navigate("/user/profile");
+    })
+    .catch((errProfile) => {
+      console.log(errProfile);
+    });
+};
 
 export const logout = () => {
-  localStorage.removeItem('user');
-}
+  localStorage.removeItem("user");
+};
