1cb1fab209497aa2b4ae9c3d8547cce6f9e18b34
Alex Mant <al.mantione@gmail.com>
2022-08-31 18:49:36 +0200

    refactor:(input editUserinfo placeholder)

--- app/src/components/MainUserHeaderInfoName/MainUserHeaderInfoName.jsx
+++ app/src/components/MainUserHeaderInfoName/MainUserHeaderInfoName.jsx
@@ -1,18 +1,24 @@
 import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getEditedFirstName, getEditedLastName } from '../../feature/loginSlice';
 import './styles.scss';
 
 const MainUserHeaderInfoName = ({isTheButtonNameEditClicked, userInfoName}) => {
-  const firstNameHasEditedTo = useRef()
-  const lastNameHasEditedTo = useRef()
+  const currentUserInfo = {
+    firstName: useSelector(state => state.login.firstName),
+    lastName: useSelector(state => state.login.lastName)
+  }
+
+  const editedFirstName = useRef()
+  const editedLastName = useRef()
+
   const dispatch = useDispatch();
 
   return(
     <>
       <div className="inputContainer" style={{display : isTheButtonNameEditClicked ? 'flex' : 'none'}}>
-        <input ref={firstNameHasEditedTo} onBlur={() => {dispatch(getEditedFirstName(firstNameHasEditedTo.current.value))}} className='nameInput' type='text' placeholder='name'/>
-        <input ref={lastNameHasEditedTo} onBlur={() => {dispatch(getEditedLastName(lastNameHasEditedTo.current.value))}} className='lastnameInput' type='text'placeholder='surname'/>
+        <input ref={editedFirstName} onBlur={() => {dispatch(getEditedFirstName(editedFirstName.current.value))}} className='nameInput' type='text' placeholder={currentUserInfo.firstName}/>
+        <input ref={editedLastName} onBlur={() => {dispatch(getEditedLastName(editedLastName.current.value))}} className='lastnameInput' type='text'placeholder={currentUserInfo.lastName}/>
       </div>
       <h2 style={{display : isTheButtonNameEditClicked ? 'none' : 'block'}}>{userInfoName + '!'}</h2>
     </>
